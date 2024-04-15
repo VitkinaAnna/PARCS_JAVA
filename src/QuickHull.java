397e5c3b9de4ee8b7a48419e43c1d96b01b364a7
VitkinaAnna <94934830+VitkinaAnna@users.noreply.github.com>
2024-04-15 10:11:24 +0300

    Update QuickHull.java

--- src/QuickHull.java
+++ src/QuickHull.java
@@ -10,8 +10,88 @@ public class QuickHull implements AM {
     private static long startTime = 0;
     private static final BigInteger MODULE = new BigInteger("2147483647");
     private static final BigInteger BASE =  new BigInteger("31");
+        public static int findSide(ArrayList<Integer> p1, ArrayList<Integer> p2, ArrayList<Integer> p) {
+        int val = (p.get(1) - p1.get(1)) * (p2.get(0) - p1.get(0)) - (p2.get(1) - p1.get(1)) * (p.get(0) - p1.get(0));
+
+        if (val > 0) {
+            return 1;
+        } else if (val < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int lineDist(ArrayList<Integer> p1, ArrayList<Integer> p2, ArrayList<Integer> p) {
+        return Math.abs((p.get(1) - p1.get(1)) * (p2.get(0) - p1.get(0)) - (p2.get(1) - p1.get(1)) * (p.get(0) - p1.get(0)));
+    }
+
+    public static void quickHull(ArrayList<ArrayList<Integer>> points, ArrayList<Integer> p1, ArrayList<Integer> p2, int side, Set<String> hull) {
+        int ind = -1;
+        int max_dist = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            ArrayList<Integer> p = points.get(i);
+            int temp = lineDist(p1, p2, p);
+
+            if ((findSide(p1, p2, p) == side) && (temp > max_dist)) {
+                ind = i;
+                max_dist = temp;
+            }
+        }
+
+        if (ind == -1) {
+            hull.add(p1.get(0) + "$" + p1.get(1));
+            hull.add(p2.get(0) + "$" + p2.get(1));
+            return;
+        }
+
+        quickHull(points, points.get(ind), p1, -findSide(points.get(ind), p1, p2), hull);
+        quickHull(points, points.get(ind), p2, -findSide(points.get(ind), p2, p1), hull);
+    }
+
+    public static ArrayList<ArrayList<Integer>> printHull(ArrayList<ArrayList<Integer>> points) {
+        Set<String> hull = new HashSet<>();
+        ArrayList<ArrayList<Integer>> hullPoints = new ArrayList<>();
+
+        if (points.size() < 3) {
+            System.out.println("Convex hull not possible");
+            return hullPoints;
+        }
+
+        ArrayList<Integer> min_x = points.get(0);
+        ArrayList<Integer> max_x = points.get(0);
+        for (int i = 1; i < points.size(); i++) {
+            ArrayList<Integer> p = points.get(i);
+            if (p.get(0) < min_x.get(0)) {
+                min_x = p;
+            }
+            if (p.get(0) > max_x.get(0)) {
+                max_x = p;
+            }
+        }
+
+        quickHull(points, min_x, max_x, 1, hull);
+        quickHull(points, min_x, max_x, -1, hull);
+
+        for (String element : hull) {
+            String[] parts = element.split("\\$");
+            int x = Integer.parseInt(parts[0]);
+            int y = Integer.parseInt(parts[1]);
+            ArrayList<Integer> hullPoint = new ArrayList<>();
+            hullPoint.add(x);
+            hullPoint.add(y);
+            hullPoints.add(hullPoint);
+        }
+
+        return hullPoints;
+    }
 	 
-	public static BigInteger computeHash(String str) {
+	public static ArrayList<ArrayList<Integer>> computeHash(String str) {
+		 ArrayList<ArrayList<Integer>> points = new ArrayList<>(Arrays.asList(
+                new ArrayList<>(Arrays.asList(0, 0)),
+                new ArrayList<>(Arrays.asList(0, 0))
+       		 ));
 		 BigInteger hashValue = BigInteger.ZERO;
 	     BigInteger powBase = BigInteger.ONE;
 
@@ -21,7 +101,7 @@ public class QuickHull implements AM {
 	         hashValue = (hashValue.add(charValue.multiply(powBase).mod(MODULE))).mod(MODULE);
 	         powBase = powBase.multiply(BASE).mod(MODULE);
 	        }
-	        return hashValue;
+	        return points;
 	 }
 
 
@@ -74,16 +154,20 @@ public class QuickHull implements AM {
       
         BigInteger[] sub_hash = new BigInteger[n];
         for (int i = 0; i < n; i++) {
-        	sub_hash[i] = (BigInteger) channels[i].readObject();
+        	sub_hash[i] = (ArrayList<ArrayList<Integer>>) channels[i].readObject();
+		// Printing the initialized 2x2 ArrayList
+        	for (ArrayList<Integer> row : sub_hash[i]) {
+        	    System.err.println(row);
+     		   }
         }
 
         System.err.println("Calculation of the result");
      
-        BigInteger hash = resultСalculation(sub_hash, sub_len);
+        //BigInteger hash = resultСalculation(sub_hash, sub_len);
        
  	long endTime = System.nanoTime();
 	
-        System.out.println("Result: " + hash.toString());
+        //System.out.println("Result: " + hash.toString());
        
         
         long timeElapsed = endTime - startTime;
@@ -98,7 +182,7 @@ public class QuickHull implements AM {
     public void run(AMInfo info) {
      
         String substring = (String)info.parent.readObject();
-        BigInteger subhash = computeHash(substring);
+         ArrayList<ArrayList<Integer>> subhash = computeHash(substring);
 
         info.parent.write(subhash);
   
